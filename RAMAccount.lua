b39a0c0216ea38427d912cbbaf905850f5a4d644
KingIcyCream <167630575+KingIcyCream1@users.noreply.github.com>
2025-11-02 02:20:37 -0600

    Refactor RAMAccount.lua for improved readability

--- RAMAccount.lua
+++ RAMAccount.lua
@@ -1,117 +1,147 @@
--- DOCUMENTATION: https://ic3w0lf22.gitbook.io/roblox-account-manager/
+local Account = {}
+Account.__index = Account
 
-local Account = {} Account.__index = Account
+local WebserverSettings = { Port = "7963", Password = "" }
 
-local WebserverSettings = {
-    Port = '7963',
-    Password = ''
-}
+function WebserverSettings:SetPort(port)
+	self.Port = tostring(port)
+end
 
-function WebserverSettings:SetPort(Port) self.Port = Port end
-function WebserverSettings:SetPassword(Password) self.Password = Password end
+function WebserverSettings:SetPassword(password)
+	self.Password = password or ""
+end
 
-local HttpService = game:GetService'HttpService'
+local HttpService = game:GetService("HttpService")
 local Request = (syn and syn.request) or request or (http and http.request) or http_request
 
-local function GET(Method, Account, ...)
-    local Arguments = {...}
-    local Url = 'http://localhost:' .. WebserverSettings.Port .. '/' .. Method .. '?Account=' .. Account
-
-    for Index, Parameter in pairs(Arguments) do
-        if typeof(Parameter) == 'boolean' then continue end
-        
-        Url = Url .. '&' .. Parameter
-    end
-
-    if WebserverSettings.Password and #WebserverSettings.Password >= 6 then
-        Url = Url .. '&Password=' .. WebserverSettings.Password
-    end
-    
-    local Response = Request {
-        Method = 'GET',
-        Url = Url
-    }
-
-    if Response.StatusCode ~= 200 then return false end
-
-    return Response.Body
+local function encode(str)
+	return HttpService:UrlEncode(tostring(str))
 end
 
-local function POST(Method, Account, Body, ...)
-    local Arguments = {...}
-    local Url = 'http://localhost:' .. WebserverSettings.Port .. '/' .. Method .. '?Account=' .. Account
-
-    for Index, Parameter in pairs(Arguments) do
-        Url = '&' .. Url .. Parameter
-    end
-
-    if WebserverSettings.Password and #WebserverSettings.Password >= 6 then
-        Url = Url .. '&Password=' .. WebserverSettings.Password
-    end
-    
-    local Response = Request {
-        Method = 'POST',
-        Url = Url,
-        Body = Body
-    }
-
-    if Response.StatusCode ~= 200 then return false end
-
-    return Response.Body
+local function buildUrl(method, account, args)
+	local url = string.format("http://localhost:%s/%s?Account=%s", WebserverSettings.Port, method, account)
+	for _, arg in ipairs(args) do
+		if typeof(arg) ~= "boolean" then
+			url ..= "&" .. arg
+		end
+	end
+	if #WebserverSettings.Password >= 6 then
+		url ..= "&Password=" .. encode(WebserverSettings.Password)
+	end
+	return url
 end
 
-function Account.new(Username, SkipValidation)
-    local self = {} setmetatable(self, Account)
-
-    local IsValid = SkipValidation or GET('GetCSRFToken', Username)
-
-    if not IsValid or IsValid == 'Invalid Account' then return false end
-
-    self.Username = Username
-
-    return self
+local function GET(method, account, ...)
+	local url = buildUrl(method, account, { ... })
+	local response = Request({ Method = "GET", Url = url })
+	if not response or response.StatusCode ~= 200 then return false end
+	return response.Body
 end
 
-function Account:GetCSRFToken() return GET('GetCSRFToken', self.Username) end
-
-function Account:BlockUser(Argument)
-    if typeof(Argument) == 'string' then
-        return GET('BlockUser', self.Username, 'UserId=' .. Argument)
-    elseif typeof(Argument) == 'Instance' and Argument:IsA'Player' then
-        return self:BlockUser(tostring(Argument.UserId))
-    elseif typeof(Argument) == 'number' then
-        return self:BlockUser(tostring(Argument))
-    end
+local function POST(method, account, body, ...)
+	local url = buildUrl(method, account, { ... })
+	local response = Request({ Method = "POST", Url = url, Body = body })
+	if not response or response.StatusCode ~= 200 then return false end
+	return response.Body
 end
-function Account:UnblockUser(Argument)
-    if typeof(Argument) == 'string' then
-        return GET('UnblockUser', self.Username, 'UserId=' .. Argument)
-    elseif typeof(Argument) == 'Instance' and Argument:IsA'Player' then
-        return self:BlockUser(tostring(Argument.UserId))
-    elseif typeof(Argument) == 'number' then
-        return self:BlockUser(tostring(Argument))
-    end
+
+function Account.new(username, skipValidation)
+	local self = setmetatable({}, Account)
+	local valid = skipValidation or GET("GetCSRFToken", username)
+	if not valid or valid == "Invalid Account" then return false end
+	self.Username = username
+	return self
 end
-function Account:GetBlockedList() return GET('GetBlockedList', self.Username) end
-function Account:UnblockEveryone() return GET('UnblockEveryone', self.Username) end
 
-function Account:GetAlias() return GET('GetAlias', self.Username) end
-function Account:GetDescription() return GET('GetDescription', self.Username) end
-function Account:SetAlias(Alias) return POST('SetAlias', self.Username, Alias) end
-function Account:SetDescription(Description) return POST('SetDescription', self.Username, Description) end
-function Account:AppendDescription(Description) return POST('AppendDescription', self.Username, Description) end
+function Account:GetCSRFToken()
+	return GET("GetCSRFToken", self.Username)
+end
 
-function Account:GetField(Field) return GET('GetField', self.Username, 'Field=' .. HttpService:UrlEncode(Field)) end
-function Account:SetField(Field, Value) return GET('SetField', self.Username, 'Field=' .. HttpService:UrlEncode(Field), 'Value=' .. HttpService:UrlEncode(tostring(Value))) end
-function Account:RemoveField(Field) return GET('RemoveField', self.Username, 'Field=' .. HttpService:UrlEncode(Field)) end
+function Account:BlockUser(arg)
+	if typeof(arg) == "Instance" and arg:IsA("Player") then
+		return self:BlockUser(arg.UserId)
+	elseif typeof(arg) == "number" then
+		arg = tostring(arg)
+	end
+	if typeof(arg) == "string" then
+		return GET("BlockUser", self.Username, "UserId=" .. arg)
+	end
+end
 
-function Account:SetServer(PlaceId, JobId) return GET('SetServer', self.Username, 'PlaceId=' .. PlaceId, 'JobId=' .. JobId) end
-function Account:SetRecommendedServer(PlaceId) return GET('SetServer', self.Username, 'PlaceId=' .. PlaceId) end
+function Account:UnblockUser(arg)
+	if typeof(arg) == "Instance" and arg:IsA("Player") then
+		return self:UnblockUser(arg.UserId)
+	elseif typeof(arg) == "number" then
+		arg = tostring(arg)
+	end
+	if typeof(arg) == "string" then
+		return GET("UnblockUser", self.Username, "UserId=" .. arg)
+	end
+end
 
-function Account:ImportCookie(Token) return GET('ImportCookie', 'Cookie=' .. Token) end
-function Account:GetCookie() return GET('GetCookie', self.Username) end
-function Account:LaunchAccount(PlaceId, JobId, FollowUser, JoinVip) -- if you want to follow someone, PlaceId must be their user id
-    return GET('LaunchAccount', self.Username, 'PlaceId=' .. PlaceId, JobId and ('JobId=' .. JobId), FollowUser and 'FollowUser=true', JoinVip and 'JoinVIP=true')
+function Account:GetBlockedList()
+	return GET("GetBlockedList", self.Username)
+end
+
+function Account:UnblockEveryone()
+	return GET("UnblockEveryone", self.Username)
+end
+
+function Account:GetAlias()
+	return GET("GetAlias", self.Username)
+end
+
+function Account:GetDescription()
+	return GET("GetDescription", self.Username)
+end
+
+function Account:SetAlias(alias)
+	return POST("SetAlias", self.Username, alias)
+end
+
+function Account:SetDescription(desc)
+	return POST("SetDescription", self.Username, desc)
+end
+
+function Account:AppendDescription(desc)
+	return POST("AppendDescription", self.Username, desc)
+end
+
+function Account:GetField(field)
+	return GET("GetField", self.Username, "Field=" .. encode(field))
+end
+
+function Account:SetField(field, value)
+	return GET("SetField", self.Username, "Field=" .. encode(field), "Value=" .. encode(value))
+end
+
+function Account:RemoveField(field)
+	return GET("RemoveField", self.Username, "Field=" .. encode(field))
+end
+
+function Account:SetServer(placeId, jobId)
+	return GET("SetServer", self.Username, "PlaceId=" .. placeId, "JobId=" .. (jobId or ""))
+end
+
+function Account:SetRecommendedServer(placeId)
+	return GET("SetServer", self.Username, "PlaceId=" .. placeId)
+end
+
+function Account:ImportCookie(token)
+	return GET("ImportCookie", "Cookie=" .. encode(token))
+end
+
+function Account:GetCookie()
+	return GET("GetCookie", self.Username)
+end
+
+function Account:LaunchAccount(placeId, jobId, followUser, joinVip)
+	return GET("LaunchAccount", self.Username,
+		"PlaceId=" .. placeId,
+		jobId and ("JobId=" .. jobId),
+		followUser and "FollowUser=true",
+		joinVip and "JoinVIP=true"
+	)
 end
 
 return Account, WebserverSettings
